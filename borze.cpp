5ca1cf79be975fc55d1ab0a8aaa0a64c48b05323
Daffa Randika <daffa.randika.3@gmail.com>
2022-12-13 09:00:45 +0700

    borze and queueAtSchool

---
+++ borze.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string borze;
+    string res;
+    cin >> borze;
+    
+    for (int i = 0; i < borze.length(); i++) {
+        if (borze[i] == '.'){
+            res += '0';
+        } else { // if borze[i] == '-'
+            if (borze[i + 1] == '.'){
+                res += '1';
+                i += 1;
+            } else {
+                res += '2';
+                i += 1;
+            }
+        }
+    }
+    cout << res << "\n";
+    return 0;
+}
